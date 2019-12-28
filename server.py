51c5cec9fcb6327e2291877f755334afde9d47fe
bauuuu1021 <j851021@gmail.com>
2019-12-28 19:09:08 +0800

    Adjust framework

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import cv2
 import numpy as np
 from hdfs import InsecureClient
 
+host = '0.0.0.0'
+port = 12345
+client_num = 1
+
 def recvall(sock, count):
     buf = b''
     while count:
@@ -12,32 +16,44 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
-host = '0.0.0.0'
-port = 12345
+def main():
+    # socket initial setting
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(client_num)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host, port))
-sock.listen(15)
+    for _ in range(client_num):
+        client_sock, client_addr = sock.accept()
+        filename = client_sock.recv(6)
+        print(filename)
 
-for _ in range(5):
-    client_sock, client_addr = sock.accept()
-    filename = client_sock.recv(6)
-    print(filename)
-    export_video = cv2.VideoWriter(filename.decode('utf-8'),cv2.VideoWriter_fourcc(*'mp4v'), 60, (1280,720))
-    while (True):
-        # recv from stream client
-        tmp = client_sock.recv(16)
-        if (tmp==b''):	# transmission done
-            break
+        result_txt = (filename.decode('utf-8')).split('.', 1)
+        detect_result = open(result_txt[0]+'.txt', 'w')
+        export_video = cv2.VideoWriter(filename.decode('utf-8'),cv2.VideoWriter_fourcc(*'mp4v'), 60, (1280,720))
+        while (True):
+            # recv from stream client
+            tmp = client_sock.recv(16)
+            if (tmp==b''):	# transmission done
+                break
+            
+            # decode image
+            img_size = int((tmp).decode('utf-8'))
+            imgstr = recvall(client_sock, img_size)  
+            img = cv2.imdecode((np.fromstring(imgstr,dtype='uint8')),1)
 
-        img_size = int((tmp).decode('utf-8'))
-        imgstr = recvall(client_sock, img_size)  
-        img = cv2.imdecode((np.fromstring(imgstr,dtype='uint8')),1)
+            '''
+            # license plate detection
+            if (detect(img) == True):
+            ''' 
+            # img to video
+            export_video.write(img)
+        export_video.release()
 
-        # img to video
-        export_video.write(img)
-    export_video.release()
+        '''
+        # upload to HDFS
+        client = InsecureClient('http://master:9870', user='ubuntu')
+        client.upload('/user/ubuntu', filename.decode('utf-8'), True)
+        '''
 
-    client = InsecureClient('http://master:9870', user='ubuntu')
-    client.upload('/user/ubuntu', filename.decode('utf-8'), True)
-
+if __name__ == '__main__':
+    main()
