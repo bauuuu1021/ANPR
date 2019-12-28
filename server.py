f4fefbbaf64358c5b3ecdda858388b052467f7f0
Ubuntu <ubuntu@ip-172-31-87-218.ec2.internal>
2019-12-28 09:35:36 +0000

    Implement ransportation framework
    server -> master(hadoop) using InsecureClient

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import cv2
 import numpy as np
+from hdfs import InsecureClient
 
 def recvall(sock, count):
     buf = b''
@@ -11,26 +12,32 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
-#host = '192.168.0.100'
-host = '127.0.0.1'
+host = '0.0.0.0'
 port = 12345
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 sock.listen(15)
 
-for _ in range(4):
-	client_sock, client_addr = sock.accept()
-	filename = client_sock.recv(5)
-	print(filename)
-	while (True):
-		tmp = client_sock.recv(16)
-	
-		if (tmp==b''):	# transmission done
-			break
-		img_size = int((tmp).decode('utf-8'))
-		imgstr = recvall(client_sock, img_size)
-		img = cv2.imdecode((np.fromstring(imgstr,dtype='uint8')),1)
-		cv2.imshow('asdf', img)
-		cv2.waitKey(1)
+for _ in range(5):
+    client_sock, client_addr = sock.accept()
+    filename = client_sock.recv(6)
+    print(filename)
+    export_video = cv2.VideoWriter(filename.decode('utf-8'),cv2.VideoWriter_fourcc(*'mp4v'), 60, (1280,720))
+    while (True):
+        # recv from stream client
+        tmp = client_sock.recv(16)
+        if (tmp==b''):	# transmission done
+            break
+
+        img_size = int((tmp).decode('utf-8'))
+        imgstr = recvall(client_sock, img_size)  
+        img = cv2.imdecode((np.fromstring(imgstr,dtype='uint8')),1)
+
+        # img to video
+        export_video.write(img)
+    export_video.release()
+
+    client = InsecureClient('http://master:9870', user='ubuntu')
+    client.upload('/user/ubuntu', filename.decode('utf-8'), True)
 
